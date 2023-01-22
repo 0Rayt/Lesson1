d68177068347017d1ff934e978194e28a492f374
Egor <Ega355585@mail.ru>
2023-01-22 20:38:28 +0300

    jjjj

---
+++ main.py
@@ -0,0 +1,25 @@
+per1 = 100
+per2 = 1
+print(per2**per1)
+per3 = 16
+per4 = 2
+print(per4**per3)
+per5 = 1000
+print(per4-per5)
+print(per4+per4*per4)
+per6 = 60
+per7 = 3
+print(per6/per7)
+import math
+print(math.sqrt (144))
+per8 = 5
+per9 = 14
+print(per4+per8*per9)
+chsl1 = 66
+chsl2 = 11
+print(chsl1-chsl2)
+print(math.sqrt(64))
+
+
+
+
